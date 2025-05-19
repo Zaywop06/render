c0a865fabb8f40616b27cffaa8054bdba861a883
Zaywop06 <241271002@alumnos.utn.edu.mx>
2025-05-19 13:21:21 -0600

    Update app.py

--- app.py
+++ app.py
@@ -28,3 +28,7 @@ def actualizar():
     conn.close()
 
     return jsonify({'status': 'ok'})
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
